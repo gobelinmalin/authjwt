2aef2f6d96d84e292ec8668e94646db067b25018
Emeline <mariemeline.paus@gmail.com>
2020-01-23 12:07:07 +0100

    port

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const connection = require('./config');
-const bcrypt = require('bcrypt');
+const connection = require("./config");
+const bcrypt = require("bcrypt");
 const app = express();
-const api = require('./routes');
+const api = require("./routes");
 
-
-const bodyParser  = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-}); 
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use('/api', api); 
+app.use("/api", api);
 
-app.listen(3000, () => console.log("Server 3000"));
+app.listen(5000, () => console.log("Server 5000"));
