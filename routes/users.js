ad678d1d25124659fdfa65809a582a785783506b
Emeline <mariemeline.paus@gmail.com>
2020-06-08 13:07:32 +0200

    first commit

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const connection = require('../config')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const connection = require("../config");
 
 router.get("/", (req, res) => {
-  connection.query('SELECT * FROM users', ( err, results) => {
-    if(err) {
-      res.status(500).send('Error retrieve users')
+  connection.query("SELECT * FROM users", (err, results) => {
+    if (err) {
+      res.status(500).send("Error retrieve users");
     } else {
-      res.json(results)
+      res.json(results);
     }
-  })
+  });
 });
 
 // Route to protect
@@ -27,41 +27,40 @@ router.post("/profile", verifyToken, (req, res) => {
         authData
       });
     }
-
   });
-  res.status(403).send("Errot connection")
+  res.status(403).send("Errot connection");
 });
 
 // Create Users
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const hash = bcrypt.hashSync(req.body.password, 10);
 
   const formData = {
-    email : req.body.email,
+    email: req.body.email,
     password: hash
-  }
-  connection.query('INSERT INTO users SET ?', [ formData ], (err, results) => {
-    if(err) {
-      res.status(500).send('Error create users')
+  };
+  connection.query("INSERT INTO users SET ?", [formData], (err, results) => {
+    if (err) {
+      res.status(500).send(err);
     } else {
-      res.sendStatus(200)
+      res.sendStatus(200);
     }
-  })
-})
+  });
+});
 
 // Route to create token
 router.post("/login", (req, res) => {
   const formData = {
-    email : req.body.email,
+    email: req.body.email,
     password: req.body.password
-  }
-  connection.query('SELECT * FROM users WHERE email = ?', [formData.email], (err,user) => {
-    if(err) {
-      res.status(500).send("Error connection")
+  };
+  connection.query("SELECT * FROM users WHERE email = ?", [formData.email], (err, user) => {
+    if (err) {
+      res.status(500).send("Error connection");
     } else {
       const isSame = bcrypt.compareSync(formData.password, user[0].password);
-      if(!isSame) {
-        res.status(500).send('Error Password')
+      if (!isSame) {
+        res.status(500).send("Error Password");
       } else {
         jwt.sign({ user }, "secretkey", (err, token) => {
           // save the token in localstorage
@@ -69,12 +68,9 @@ router.post("/login", (req, res) => {
             token
           });
         });
-       
       }
-
     }
-  })
-
+  });
 });
 
 // Fomat token
@@ -82,7 +78,6 @@ router.post("/login", (req, res) => {
 
 // Verify token
 function verifyToken(req, res, next) {
-  console.log("ok")
   // Get auth header value
   const bearerHeader = req.headers["authorization"];
   //Check if bearer is undefined
